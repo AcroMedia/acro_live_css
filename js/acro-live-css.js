3fae74ebc98016ae875c96323444a76d732161b4
cbildstein <cbildstein@acromediainc.com>
2017-01-04 17:29:41 -0800

    Add module version number and update files to code standard
    - Updated Live CSS template markup for headers.

--- js/acro-live-css.js
+++ js/acro-live-css.js
@@ -1,10 +1,8 @@
 /**
- * Acro Live JS
+ * Live CSS JS.
  */
-
 (function ($, Drupal) {
-
-  // Live CSS Nav
+  // Live CSS Nav.
   if ($('.live-css-nav').length) {
     $('body').scrollspy({
       target: '.live-css-nav-col'
@@ -17,4 +15,14 @@
     });
   }
 
+  // Typography.
+  $('.print-font-family').each(function() {
+    var fontFamily = $(this).css('font-family');
+    $(this).append(' - ' + fontFamily);
+  });
+
+  $('.print-font-size').each(function() {
+    var fontSize = $(this).css('font-size');
+    $(this).append(' - ' + fontSize);
+  });
 })(jQuery, Drupal);
--- src/Controller/LiveCSSController.php
+++ src/Controller/LiveCSSController.php
@@ -16,7 +16,3 @@ class LiveCSSController extends ControllerBase {
   }
 }
 ?>
-
-
-
-
