dd6dca68698c0d687d300ade9147fa72c47ed3d4
cbildstein <cbildstein@acromedia.com>
2018-09-13 04:39:43 -0700

    Add theme check variable and remove old image assets
    - Add module readme file.
    - Add Orange E-Commerce Starter active theme check for library assets and sample markup.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Live CSS
+
+> Module by Acro Media Inc.
+
+Creates a page showcasing general site styles and commonly used elements.
+
+The module optionally uses assets from the [Orange E-Commerce Starter](https://github.com/AcroMedia/orange_ecom_starter) theme. It's recommended to have the theme enabled (it doesn't have to be set to default).
--- src/Controller/LiveCSSController.php
+++ src/Controller/LiveCSSController.php
@@ -10,9 +10,18 @@ use Drupal\Core\Controller\ControllerBase;
 
 class LiveCSSController extends ControllerBase {
   public function content() {
+    // Check if Orange E-Commerce Starter theme is enabled.
+    $orange_ecom_starter_is_active = NULL;
+    $themes_list = \Drupal::service('theme_handler')->listInfo();
+
+    if (isset($themes_list['orange_ecom_starter']) && $themes_list['orange_ecom_starter']->status == 1) {
+      $orange_ecom_starter_is_active = TRUE;
+    }
+
     $page = [
       '#type' => 'page',
       '#theme' => 'live_css',
+      '#orange_ecom_starter_is_active' => $orange_ecom_starter_is_active,
     ];
 
     return $page;
